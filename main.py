8aceaccc454d3d40858e512a42501360ecbc0af8
Marius Schmitt <schmittma@elektronikschule.de>
2025-02-18 20:37:53 +0100

    Add helper functions to append values from previous rows in the spreadsheet

--- main.py
+++ main.py
@@ -18,6 +18,27 @@ config_sheet = spreadsheet.worksheet("Konfiguration")
 playsheet_4p = spreadsheet.worksheet("Spielhistorie 4P")
 playsheet_3p = spreadsheet.worksheet("Spielhistorie 3P")
 
+def get_col_by_name(sheet, name):
+    header_row = sheet.row_values(1)
+    return header_row.index(name) + 1
+
+# Helper function to append the values from the previous row to the new row
+# HIghly dependent on the acutal contents of the sheet
+# However i was not able to get a trigger in the sheet directly to work
+def append_values_from_previous_columns(response, sheet):
+    # use join(filter(str.isdigit, ...)) To seperate the index from the letter
+    # The repsonse contains the range until the previous row
+    previous_row_index = ''.join(filter(str.isdigit, response['tableRange'].split(':')[1]))
+    new_row_index = int(previous_row_index) + 1
+    
+    target_col = get_col_by_name(sheet, "Wert")
+    
+    previous_cell = sheet.cell(int(previous_row_index), target_col, value_render_option='FORMULA')
+    
+    # Handle the cost column
+    content_to_copy = previous_cell.value.replace(previous_row_index, str(new_row_index))
+    sheet.update_cell(int(new_row_index), target_col, content_to_copy)
+
 @app.route('/sheet', methods=['GET'])
 def get_sheet_link():
     return jsonify({"link": f"https://docs.google.com/spreadsheets/d/{SERVER_CONFIG['spreadsheet_id']}"})
@@ -25,8 +46,7 @@ def get_sheet_link():
 @app.route('/users', methods=['GET'])
 def get_users():
     # Fetch all values from the "Spieler" column
-    header_row = config_sheet.row_values(1)
-    spieler_col_index = header_row.index("Spieler") + 1
+    spieler_col_index = get_col_by_name(config_sheet, "Spieler")
     spieler_col_values = config_sheet.col_values(spieler_col_index)[1:]
     return jsonify(spieler_col_values)
 
@@ -72,6 +92,8 @@ def add_score():
             
             response = playsheet_4p.append_row(new_row)
             
+            append_values_from_previous_columns(response, playsheet_4p)
+            
         else:
             new_row.extend(players)
             new_row.extend([''] * (1 - len(players)))
@@ -82,6 +104,8 @@ def add_score():
             new_row.append(date)
             
             response = playsheet_3p.append_row(new_row)
+            
+            append_values_from_previous_columns(response, playsheet_3p)
         
         if response['updates']['updatedRows'] != 1:
             return jsonify({"status": "error", "message": "Server error. Play was not submitted."}), 500
